f8fb5bdb9a253126a183cac587733dd8c11dae68
Sean Sovine <sean.r.sovine@gmail.com>
2024-03-09 17:22:02 -0500

    Refactor a bit to clean up

--- src/first_example/bessel.cpp
+++ src/first_example/bessel.cpp
@@ -23,6 +23,8 @@ constexpr double T_STEP = 0.001;
 /* Definition for GSL. */
 
 struct GslJn {
+  // Function object to wrap gsl.
+
   double operator()(double x) {
     gsl_sf_result result{};
     gsl_sf_bessel_Jn_e(BESSEL_ORDER, x, &result);
@@ -36,11 +38,8 @@ typedef std::vector<double> State_T;
 typedef std::vector<double> ResultSeq_T;
 typedef std::pair<ResultSeq_T, ResultSeq_T> Results_T;
 
-class Bessel {
+struct Bessel {
   // Function object for RHS of x' = f(x).
-
-public:
-  Bessel() {}
 
   void operator()(const State_T &x, State_T &dxdt, const double t) {
     dxdt[0] = x[1];
@@ -49,13 +48,13 @@ public:
   }
 };
 
-struct push_back_state_and_time {
+struct StateAndTimeObserver {
   // Observer (from odeint examples).
 
   std::vector<State_T> &m_states;
   std::vector<double> &m_times;
 
-  push_back_state_and_time(std::vector<State_T> &states, std::vector<double> &times)
+  StateAndTimeObserver(std::vector<State_T> &states, std::vector<double> &times)
       : m_states(states), m_times(times) {}
 
   void operator()(const State_T &x, double t) {
@@ -64,49 +63,65 @@ struct push_back_state_and_time {
   }
 };
 
-Results_T runOdeint(double xMin, double xMax, double step) {
-  using namespace boost::numeric::odeint;
+class OdeintBesselRunner {
   typedef std::vector<State_T> X_Results_T_;
 
-  GslJn gslJn{};
+  // x[0] = f(t); x[1] = f'(t)
+  State_T x;
 
-  constexpr double H = 0.00001;
+  // For estimating derivative.
+  static constexpr double H = 0.00001;
 
-  // state_initialization
-  State_T x(2);
-  x[0] = gslJn(T_MIN);
-  x[1] = (gslJn(T_MIN + H) - gslJn(T_MIN - H)) / (2 * H);
-  // Initialize w/ known value for testing.
+public:
+  OdeintBesselRunner() : x(2) {
+    GslJn gslJn{};
 
-  // Results containers.
-  X_Results_T_ x_vec;
-  ResultSeq_T times;
-
-  Bessel bf{};
-
-  // Use a simple constant stepper and integrator.
-  // This is because we want the observer to be called
-  // at regular intervals. (See odeint docs.)
-  runge_kutta4<State_T> stepper;
-  integrate_const(stepper, bf, x, xMin, xMax, step, push_back_state_and_time(x_vec, times));
-
-  ResultSeq_T resultsFOnly(size(times));
-  for (size_t i = 0; i < size(times); i++) {
-    resultsFOnly[i] = x_vec[i][0];
+    // State_initialization:
+    // Due to singularity in equation we're starting
+    // past x = 0. We initialize w/ known values from
+    // GSL for verification purposes.
+    x[0] = gslJn(T_MIN);
+    x[1] = (gslJn(T_MIN + H) - gslJn(T_MIN - H)) / (2 * H);
   }
 
-  return std::make_pair(resultsFOnly, times);
-}
+  Results_T run(double xMin, double xMax, double step) {
+    using namespace boost::numeric::odeint;
+
+    // Results containers.
+    X_Results_T_ x_vec;
+    ResultSeq_T times;
+
+    // RHS function object.
+    Bessel bf{};
+
+    // We use a constant stepper and integrator.
+    // This is because we want the observer to be called
+    // at regular intervals. (See odeint docs.)
+    runge_kutta4<State_T> stepper;
+    integrate_const(stepper, bf, x, xMin, xMax, step, StateAndTimeObserver(x_vec, times));
+
+    ResultSeq_T resultsFOnly(size(times));
+    for (size_t i = 0; i < size(times); i++) {
+      resultsFOnly[i] = x_vec[i][0];
+    }
+
+    return std::make_pair(resultsFOnly, times);
+  }
+};
 
 /* Main function. */
 
 int main() {
+  // Initialize plot.
+
   matplot::hold(matplot::on);
   matplot::grid(matplot::on);
 
   // Compute and plot using odeint.
 
-  Results_T odeintResult = runOdeint(T_MIN, T_MAX, T_STEP);
+  OdeintBesselRunner runner{};
+
+  Results_T odeintResult = runner.run(T_MIN, T_MAX, T_STEP);
 
   const ResultSeq_T &time = odeintResult.second;
   const ResultSeq_T &odeiVals = odeintResult.first;
@@ -124,5 +139,6 @@ int main() {
   // Show the plot.
 
   matplot::show();
+
   return 0;
 }
