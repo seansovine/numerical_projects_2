99108e489ac35a956f8c537ce13747c1695ea1b4
Sean Sovine <sean.r.sovine@gmail.com>
2024-06-06 22:56:31 -0400

    Move struct definition back to header (for now)
    Since this is a small project, we won't make a separate implementation
    file just for this struct's methods. The rest of the code for wrapping
    the boost solver is  templated, and would stay in a header anyway.

--- src/bessel_example/odeint_runner.h
+++ src/bessel_example/odeint_runner.h
@@ -12,8 +12,23 @@ using timeseq_t = resultseq_t;
 using results_t = std::pair<resultseq_t, timeseq_t>;
 
 namespace detail {
-struct StateAndTimeObserver;
-}
+
+// Just definining this here for now.
+// Will move definitions if the project grows larger.
+struct StateAndTimeObserver {
+  std::vector<state_t> &m_states;
+  std::vector<double> &m_times;
+
+  StateAndTimeObserver(std::vector<state_t> &states, std::vector<double> &times)
+      : m_states(states), m_times(times) {}
+
+  void operator()(const state_t &x, double t) {
+    m_states.push_back(x);
+    m_times.push_back(t);
+  }
+};
+
+} // namespace detail
 
 template <typename T>
 concept RHS = requires(T a, const state_t &x, state_t &dxdt, const double t) { a(x, dxdt, t); };
